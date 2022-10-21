fdc5137da24c56eb47d8014cda96614efb7cea81
marianoklecha <marianoklecha01@gmail.com>
2022-10-21 13:17:11 -0300

    'hOLA'

--- tests/ejemplo.test.js
+++ tests/ejemplo.test.js
@@ -1,6 +1,4 @@
-test("Tijera contra tijera", ()=>{
-    const tijera1 = new Tijera();
-    const tijera2 = new Tijera();
-    expect(tijera1.contra(tijera2)).toBe("Em pate");
+test("hOLIS", ()=>{
+    expect("hOLA MUNDO").toBe("hOLA MUNDO");
 
 })
