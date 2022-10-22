cca1285c0d208692fb6501b8eef75c01de34d4b9
Delfi Lh <delfilh@MacBook-Air-de-Delfi-Lh.local>
2022-10-22 10:38:46 -0300

    hOLA

--- tests/tablero.test.js
+++ tests/tablero.test.js
@@ -1,7 +1,12 @@
+const Tablero = require('../src/Tablero');
 const Tablero= require('../src/Tablero');
 
 test("tablero Apagado",()=>{
     const tablero= new Tablero();
     expect(tablero.estado).toBe("Apagado");
+})
+test("tablero Encedido",()=>{
+    const Tablero=new Tablero();
+    expect(tablero.estado).toBe("Encendido");
 })
 
