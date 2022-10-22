edd86265edbe248f90a16a5a2782195d10979604
Delfi Lh <delfilh@MacBook-Air-de-Delfi-Lh.local>
2022-10-22 12:04:44 -0300

    test

--- tests/tablero.test.js
+++ tests/tablero.test.js
@@ -1,24 +1,27 @@
 const Lampara = require('../src/Lampara');
 const Tablero = require('../src/Tablero');
 
+beforeAll(() => {
+    const tablero = new Tablero();
+  });
 
 test("tablero Apagado",()=>{
-    const tablero= new Tablero();
+    
     expect(tablero.estado).toBe("Apagado");
 })
 test("tablero Encedido",()=>{
-    const tablero=new Tablero();
+    
     const estado= tablero.getEstado();
     expect(tablero.estado).toBe("Encendido");
 })
 test("prender Lampara",()=>{
-    const tablero=new Tablero;
+    
     tablero.linea[0]=new Lampara();
     tablero.linea[0].prender();
     expect(tablero.linea[0].getEstado()).toBe("Encendida");
 })
 test("prender linea",()=>{
-    const tablero=new Tablero;
+   
     expect(tablero.prenderLinea()).toBe("Linea Encendida");
 
 })
