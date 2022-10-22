429e7b1d9bb530c6706b81e86632c64b94d895d8
Delfi Lh <delfilh@MacBook-Air-de-Delfi-Lh.local>
2022-10-22 11:42:41 -0300

    test

--- src/Tablero.js
+++ src/Tablero.js
@@ -1,8 +1,11 @@
 function Tablero(){
     this.estado="Apagado";
+    this.linea=new Array(100);
+    
     this.getEstado=function(){
         this.estado="Encendido";
 
     };
+
 }
 module.exports=Tablero;
