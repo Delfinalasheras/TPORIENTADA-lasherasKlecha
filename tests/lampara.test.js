60c517c289931fa9a6d1d8542cb3786b57370aea
marianoklecha <marianoklecha01@gmail.com>
2022-10-24 12:42:39 -0300

    'test'

--- tests/lampara.test.js
+++ tests/lampara.test.js
@@ -1,12 +1,12 @@
 const Lampara= require('../src/Lampara');
 test("lampara Apagada",()=>{
     const lampara= new Lampara();
-    expect(lampara.estado).toBe("Apagada");
+    expect(lampara.intensidad).toBe(0);
 })
 test("lampara Encedida",()=>{
     const lampara= new Lampara();
-    lampara.prender();
-    expect(lampara.getEstado()).toBe("Encendida");
+    lampara.encender();
+    expect(lampara.intensidad).toBe(1);
 })
 test("Cambiar intensidad",()=>{
     const lampara= new Lampara();
