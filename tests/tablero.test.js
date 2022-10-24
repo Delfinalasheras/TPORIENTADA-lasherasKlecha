4ef9e30154a2d715697f9e3a2b0606116f9ace0a
marianoklecha <marianoklecha01@gmail.com>
2022-10-24 16:29:25 -0300

    'test'

--- tests/tablero.test.js
+++ tests/tablero.test.js
@@ -1,45 +1,40 @@
 const Lampara = require('../src/Lampara');
 const Tablero = require('../src/Tablero');
 
+var tablero;
+beforeEach(() => {
+    tablero = new Tablero();
+});
 
 test("tablero Apagado",()=>{
-    const tablero = new Tablero();
     expect(tablero.estado).toBe("Apagado");
 })
 test("tablero Encedido",()=>{
-    const tablero = new Tablero();
     tablero.armarTablero();
     tablero.prenderCompleto();
     expect(tablero.estado).toBe("Tablero Encendido");
 })
 test("prender Lampara",()=>{
-    const tablero = new Tablero();
     tablero.linea[0]=new Lampara();
     tablero.linea[0].encender();
     expect(tablero.linea[0].intensidad).toBe(1);
 })
 test("prender linea",()=>{
-    const tablero = new Tablero();
     expect(tablero.prenderLinea()).toBe("Linea Encendida");
-
 })
 test("prender Posicion",()=>{
-    const tablero = new Tablero();
     tablero.armarTablero();
     tablero.prenderPosicion([1,2],[3,2]);
     tablero.prenderPosicion([1,2],[3,4]);
     expect(tablero.estado).toBe("cuadrado prendido");
 })
 test("Apagar Posicion",()=>{
-    const tablero = new Tablero();
     tablero.armarTablero();
     tablero.prenderPosicion([1,2],[3,2]);
     tablero.apagarPosicion([1,2],[3,2]);
     expect(tablero.estado).toBe("cuadrado apagado");
 })
 test("Cambiar Estado Posicion",()=>{
-    const tablero = new Tablero();
-
     tablero.armarTablero();
     tablero.prenderPosicion([1,2],[3,2]);
     tablero.apagarPosicion([1,2],[1,2]);
@@ -47,19 +42,16 @@ test("Cambiar Estado Posicion",()=>{
     expect(tablero.estado).toBe("cuadrado cambiado");
 })
 test("Luces Prendidas",()=>{
-    const tablero=new Tablero();
     tablero.armarTablero();
     tablero.prenderPosicion([1,2],[3,2]);
     tablero.apagarPosicion([2,2],[2,2]);
     tablero.cambiarEstados([1,2],[3,2]);
-    
     expect(tablero.contarPrendidas()).toBe(3);
 
 })
 test("Posición negativa",()=>{
-    const tablero = new Tablero();
     tablero.armarTablero();
-    expect(tablero.verificarPosicion([-1,2],[3,2])).toThrow(new Error("La posición debe ser positiva"));
+    expect(()=>{tablero.verificarPosicion([-1,2],[3,2])}).toThrow(new Error("La posición debe ser positiva"));
 })
 
     
